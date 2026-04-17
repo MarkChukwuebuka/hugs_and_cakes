c5a8da9ca155b304457eac1aafe4b311457bbe64
Mark Okonedah <markedah9652@gmail.com>
2026-04-17 10:42:06 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ pip install -r requirements.txt
 
 # Collect static files
 echo "🎨 Collecting static files..."
-#python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Run migrations
 echo "🗄️  Running database migrations..."
