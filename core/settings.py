adc4b77f730f23b0f308f058cadd5916eadd571f
Mark Okonedah <markedah9652@gmail.com>
2026-04-18 16:56:14 +0100

    update

--- core/settings.py
+++ core/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL")
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv("DATABASE_URL")
 
-    )
-}
+        )
+    }
 
 
 # Password validation
